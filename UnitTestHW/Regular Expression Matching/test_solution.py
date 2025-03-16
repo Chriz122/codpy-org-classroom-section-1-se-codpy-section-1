be16121128a5bdc6220be04152aa922e3ea5971a
chris <chriszeng921229@gmail.com>
2025-03-16 16:09:37 +0800

    完成UnitTestHW

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,26 +1,25 @@
-import ...
-from ...
+import pytest
+import solution
 
 @pytest.fixture
-...
-    
+def solution():
+    return solution()
+
 testcases = [
     # s, p, expected_res
-    ["", "", True],
-    ["aa", "a", False],
-    ["aa", "a*", True],
-    ["ab", ".*", True],
-    ["a", ".*.", True],
-    ["aab", "c*a*b", True],
-    ["aaa", "ab*a*c*a", True]
+    ("", "", True),
+    ("aa", "a", False),
+    ("aa", "a*", True),
+    ("ab", ".*", True),
+    ("a", ".*.", True),
+    ("aab", "c*a*b", True),
+    ("aaa", "ab*a*c*a", True)
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
+@pytest.mark.parametrize("s, p, expected", testcases)
+def test_isMatch(solution, s, p, expected):
     assert solution.isMatch(s, p) == expected
 
-"""
 @pytest.mark.xfail
 def test_broken_solution(solution):
-    assert solution.isMatch() == 
-"""
+    assert solution.isMatch("some", "broken") == False
